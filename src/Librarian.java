31f974815279d9a0847f8443fde644f51c2636fa
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-16 15:59:19 -0500

    Added printDueDate function to Librarian.java

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by Victoria Owen on 2/15/2017.
  */
 public class Librarian {
 
-    
+    /**
+     * YK: Constructor, does nothing
+     */
+    public Librarian() {
+        // Do nothing
+    }
 
     /**
      * YK - Prints a numbered list according to the ArrayList returned by LibraryDataBase
@@ -21,6 +27,7 @@ public class Librarian {
         return;
     }
 
+
     /**
      * YK - This method will allow the librarian to check out a media according to the user input index
      * @param list Currently displayed list
@@ -34,11 +41,36 @@ public class Librarian {
 
     /**
      * YK - This method will allow the librarian to check in a media according to the user input index
-     * @param list
-     * @param index
+     * @param list Currently displayed list
+     * @param index Choice of user, objects using this class should be careful
      */
     public void checkInMedia(ArrayList<Media> list, int index) {
         list.get(index).checkInMedia();
+        return;
+    }
+
+
+    /**
+     * YK - This method will print the due date of an object
+     * @param list Currently displayed list
+     * @param index Choice of user, objects using this class should be careful
+     */
+    public void printDueDate(ArrayList<Media> list, int index) {
+        System.out.print("The due date for this item is: ");
+        System.out.print(list.get(index).getDueDate().get(Calendar.MONTH) + "-");
+        System.out.print(list.get(index).getDueDate().get(Calendar.DAY_OF_MONTH) + "-");
+        System.out.print(list.get(index).getDueDate().get(Calendar.YEAR));
+    }
+
+    
+    /**
+     * YK - This method provides
+     * @param list
+     * @param index
+     */
+    public void lookUpMediaInfo(ArrayList<Media> list, int index) {
+        // TODO: Maybe this can provide more information about an object
+        list.get(index).toString();
         return;
     }
 
