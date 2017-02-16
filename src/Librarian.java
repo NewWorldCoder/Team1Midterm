8d3662bd4f12d693ba8a541dc8fe59e320377fb7
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-16 15:14:13 -0500

    Cleaned up librarian

--- src/Librarian.java
+++ src/Librarian.java
@@ -5,24 +5,7 @@ import java.util.ArrayList;
  */
 public class Librarian {
 
-    private Media m;
-    private LibraryDataBase ldb;
-
-    public Media getM() {
-        return m;
-    }
-
-    public void setM(Media m) {
-        this.m = m;
-    }
-
-    public LibraryDataBase getLdb() {
-        return ldb;
-    }
-
-    public void setLdb(LibraryDataBase ldb) {
-        this.ldb = ldb;
-    }
+    
 
     /**
      * YK - Prints a numbered list according to the ArrayList returned by LibraryDataBase
@@ -59,28 +42,5 @@ public class Librarian {
         return;
     }
 
-    
-    // duplicated from LibraryDatabase.java to return a string type to use to create check out/check in
-    public String printMediaDatabase2(){
-        String med = LibraryDataBase.readTextFromFile("docs/Team1LibraryDbase.csv");
-
-
-        String[] byLine = med.split("\\n");
-        String[] columns;
-        ArrayList<Media> mediaList = new ArrayList<Media>();
-
-        for(int i = 1;i<=19;i++)
-        {
-            columns = byLine[i].split(","); //populates columns array with split of each line with a comma delimiter
-
-            mediaList.add(new Book(columns[1], columns[2],columns[3]));//populates mediaList with Book objects
-
-
-            System.out.println(mediaList.get(i-1).getAuthor()); // Prints book objects to console.
-
-
-        }
-        return mediaList.toString();
-    }
 
 }
