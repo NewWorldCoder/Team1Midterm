14ee882521237ee8a1925c1b93d145529f0f32c2
Mike <vital39@yahoo.com>
2017-02-16 11:02:37 -0500

    Test push

--- src/LibraryDataBase.java
+++ src/LibraryDataBase.java
@@ -8,54 +8,44 @@ import java.util.ArrayList;
 
 public class LibraryDataBase {
 
-
-
-
     public static void printMediaDatabase(){
     String med = readTextFromFile("docs/Team1LibraryDbase.csv");
 
 
     String[] byLine = med.split("\\n");
     String[] columns;
-        ArrayList<Media> mediaList = new ArrayList<Media>();
-
-
-
-
-
+    ArrayList<Media> mediaList = new ArrayList<Media>();
 
     for(int i = 1;i<=19;i++)
     {
-        columns = byLine[i].split(",");
+        columns = byLine[i].split(","); //populates columns array with split of each line with a comma delimiter
 
-            mediaList.add(new Book(columns[1], columns[2],columns[3]));
-            
+        mediaList.add(new Book(columns[1], columns[2],columns[3]));//populates mediaList with Book objects
 
 
+        System.out.println(mediaList.get(i-1).getAuthor()); // Prints book objects to console.
 
 
     }
 
+}
 
-
-
-//
-//    String[][] columns = new String[byLine.length][];
-//
-//
-//
-//    for( int i = 1; i< byLine.length; i++) //skipping first line because it's just book,
-//
-//    {
-//        for(int j = 0; j<byLine[i].length;j++)
+//    public ArrayList getAllAuthor(String author){
+//        ArrayList<Media> mediaList = new ArrayList<Media>();
+//        ArrayList<Media> mediaListAuthors = new
+//        for(int i = 1;i<=19;i++)
 //        {
-//            columns[i][j]=byLine[i].split(",");
+//           if(mediaList.get(i).getAuthor().equalsIgnoreCase(author)){
+//               mediaListAuthors.add(mediaList.get(i))
+//
+//           }
+//
 //
 //        }
 //
+//
+//
 //    }
-
-}
 
 
 
