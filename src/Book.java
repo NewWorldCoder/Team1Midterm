020d69cdfc538328ab06da9b43f5449ae1600191
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-16 09:10:02 -0500

    First edit for Book.java and Media.java.  Initial set of constructors, setters, and getters added."

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,18 @@
 /**
  * Created by Mike on 2/15/2017.
  */
-public class Book {
+public class Book extends Media {
+
+    /**
+     * Constructor for Book objects
+     * @param title Title for Book object
+     * @param category Category for Book object
+     * @param author Author for Book object
+     */
+    public Book (String title, String category, String author) {
+        super.setTitle(title);
+        super.setCategory(category);
+        super.setAuthor(author);
+        super.setType(MediaType.BOOK);
+    }
 }
--- src/Media.java
+++ src/Media.java
@@ -1,5 +1,82 @@
+import java.util.Date;
+
 /**
- * Created by Mike on 2/15/2017.
+ * Created by Yosuke on 2/15/2017.
  */
-public class Media {
+abstract class Media {
+
+    // Data members
+    private String title;                                                    // Holds the title of the Media object
+    private String category;                                                 // Holds the category of the Media object
+    private String author;                                                   // Holds the author of the Media object
+    static enum MediaStatus {ON_SHELF, CHECKED_OUT, OVERDUE, ON_HOLD};       // Enum for status of the Media object
+    private MediaStatus status;                                              // Status for Media Object
+    private Date checkoutDate;                                               // Holds the checkout date
+    private Date dueDate;                                                    // Holds the due date
+    static enum MediaType {BOOK, CD, DVD, AUDIO_BOOK, PERIODICAL, REFERENCE} // Enum for the type of the Media object
+    private MediaType type;
+
+
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private MediaStatus getStatus() {
+        return status;
+    }
+
+    private void setStatus(MediaStatus status) {
+        this.status = status;
+    }
+
+    private Date getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    private void setCheckoutDate(Date checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public MediaType getType() {
+        return type;
+    }
+
+    protected void setType(MediaType type) {
+        this.type = type;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+
+
+
+
 }
