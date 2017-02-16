c42b237f5021775528979586b92e5224d6cb3eca
NewWorldCoder <rosinaf@aol.com>
2017-02-16 15:19:51 -0500

    updated LibraryApp

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -5,15 +5,17 @@ public class LibraryApp {
 
     public static void main(String[] args) {
 
-
+        Scanner scan1 = new Scanner(System.in);
         LibraryDataBase ldb = new LibraryDataBase("docs/Team1LibraryDbase.csv");
         Librarian librarian = new Librarian();
         ArrayList<Media> list = new ArrayList<>();
 
 
-
-
         int userInput;
+        String authorInput;
+        String titleInput;
+        String mediaInput;
+
 
 
         System.out.println("Welcome to the Grand Circus Library.");
@@ -22,8 +24,8 @@ public class LibraryApp {
         System.out.println("2. Display All Available");
         System.out.println("3. Display All Author Search");
         System.out.println("4. Display All Title Search");
-        System.out.println("5. Display All Media Search");
-        System.out.println("6. Donate (Add)");
+      //  System.out.println("5. Display All Media Search");
+      //  System.out.println("6. Donate (Add)");
 
         userInput = InputValidator.getValidInteger(1, 6);
 
@@ -36,17 +38,28 @@ public class LibraryApp {
                 list = ldb.getAllAvailable();
                 librarian.printArray(list);
                 break;
-           /* case 3:
-
+            case 3:
+                System.out.println("Enter an author you'd like us to search for.");
+                authorInput= scan1.nextLine();
+                list = ldb.getAllAuthor(authorInput);
+                librarian.printArray(list);
                 break;
             case 4:
-
+                System.out.println("Enter a title you'd like us to search for.");
+                titleInput = scan1.nextLine();
+                list = ldb.getAllTitle(titleInput);
+                librarian.printArray(list);
                 break;
-            case 5:
+         /*   case 5:
+                list = ldb.getAllMedia("");
+                ldb.getAllMedia();
+                break;
+            case 6:
+            dd*/
 
-                break;*/
 
 
         }
+        
     }
 }
