3970deb7ab9a7e52dcff3f01ebd73c0c52d67d7b
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-16 22:11:02 -0500

    Added more comments into files

--- src/MediaStatus.java
+++ src/MediaStatus.java
@@ -1,8 +1,14 @@
 /**
+ * This is the enumeration for the media statuses
  * Created by yosuk on 2/16/2017.
  */
 public enum MediaStatus {
     ON_SHELF, CHECKED_OUT, OVERDUE, ON_HOLD;        // Enum for status of the Media object
+
+    /**
+     * This method will return a string that represents the enum
+     * @return String that represents the enum
+     */
     @Override
     public String toString() {
         switch (this) {
--- src/MediaType.java
+++ src/MediaType.java
@@ -1,9 +1,14 @@
 /**
+ * This is the enumeration for the media types.
  * Created by yosuk on 2/16/2017.
  */
 public enum MediaType {
     BOOK, CD, DVD, AUDIO_BOOK, PERIODICAL, REFERENCE;
 
+    /**
+     * This method will return a string that represents the enum
+     * @return String that represents the enum
+     */
     @Override
     public String toString() {
         switch (this) {
